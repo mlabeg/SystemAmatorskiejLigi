78ef57464b31f26591fbc90c6994c5dad442ce31
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-09-25 23:52:43 +0200

    utworzenie i przeniesienie logiki od GamesController

---
+++ LigaAmatorska.Presentation/Controllers/GamesController.cs
@@ -0,0 +1,48 @@
+﻿using LigaAmatorska.Application.Services;
+using LigaAmatorska.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LigaAmatorska.Presentation.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly IMeczService _meczService;
+
+        public GamesController(IMeczService meczService)
+        {
+            _meczService = meczService;
+        }
+
+        [HttpGet]
+        public IActionResult DodajMecz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DodajMecz(Mecz mecz)
+        {
+            await _meczService.DodajMeczAsync(mecz);
+            return RedirectToAction("Harmonogram");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AktualizujWynikMeczu(int id)
+        {
+            var mecz = await _meczService.GetByIdAsync(id);
+            return View(mecz);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AktualizujWynikMeczu(Mecz mecz)
+        {
+            if (ModelState.IsValid)
+            {
+                await _meczService.AktualizujWynikAsync(mecz.Id, mecz.WynikA, mecz.WynikB);
+                return RedirectToAction("Harmonogram");
+            }
+            return View(mecz);
+        }
+    }
+}
